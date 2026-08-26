ad398554e36cf64bdb17c091d30a9189117bb45b
Abhishek Baghel <abhipal92013@gmail.com>
2026-08-26 16:33:54 +0530

    Add files via upload

---
+++ Float.java
@@ -0,0 +1,9 @@
+public class Float {
+    public static void main(String[] args) {
+        float a = 10.5f, b = 20.5f;
+        float sum = a + b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println("sum: " + sum);
+    }
+}
---
+++ String.java
@@ -0,0 +1,6 @@
+public class String {
+    public static void main(String[] args) {
+        String str = "Hello World!";
+        System.out.println(str);
+    }
+}
