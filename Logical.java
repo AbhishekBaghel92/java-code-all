188b814cb1800b94ad29b1c985764838ad477bcf
Abhishek Baghel <abhipal92013@gmail.com>
2026-09-01 18:02:18 +0530

    Add files via upload

---
+++ Logical.java
@@ -0,0 +1,12 @@
+package Operator;
+
+public class Logical {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println("a && b: " + (a && b)); // Logical AND
+        System.out.println("a || b: " + (a || b)); // Logical OR
+        System.out.println("!a: " + (!a));         // Logical NOT
+    }
+}
