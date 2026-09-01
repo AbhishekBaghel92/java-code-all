963efab028173930d976295dd83e68c66487f1d9
Abhishek Baghel <abhipal92013@gmail.com>
2026-09-01 18:12:17 +0530

    Add files via upload

--- PrintStart.java
+++ PrintStart.java
@@ -1,16 +1,16 @@
 package WhileLoop;
 
-public class PrintNumber {
+public class PrintStart {
     public static void main(String[] args) {
-        int i = 5;
-        while (i >= 1) {
+        int i = 1;
+        while (i <= 5) {
             int j = 1;
             while (j <= i) {
-                System.out.print(i);
+                System.out.print("*");
                 j++;
             }
             System.out.println();
-            i--;
+            i++;
         }
     }
 }
--- ThreeTable.java
+++ ThreeTable.java
@@ -1,10 +1,10 @@
 package WhileLoop;
 
-public class Hello {
+public class ThreeTable {
     public static void main(String[] args) {
         int i = 1;
-        while (i <= 5) {
-            System.out.println("Hello, World!");
+        while (i <= 10) {
+            System.out.println("3 x " + i + " = " + (3 * i));
             i++;
         }
     }
